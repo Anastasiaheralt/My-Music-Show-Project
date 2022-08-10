222b4ae1f0f90445f2ae871490e357612b8b8bc5
Jechf <jesuse.chf@gmail.com>
2022-08-10 18:57:34 +0000

    cards details services

---
+++ src/front/js/component/services.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+export const Services = () => {
+  return (
+    <div class="row">
+      <div class="col-sm-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Locaciones</h5>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Música</h5>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Detalles
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Fotografía</h5>
+            <p class="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" class="btn btn-primary">
+              Detalles
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
