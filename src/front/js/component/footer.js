d8a3ceb215105fb2a95e91afa15bb3a2bb8a4118
Jechf <jesuse.chf@gmail.com>
2022-08-10 03:25:25 +0000

    limpieza de codigo, navbar, footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,24 @@
 import React, { Component } from "react";
+import IG from "../../img/IG.png";
+import FB from "../../img/FB.png";
+import TW from "../../img/TW.png";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center bg-light">
+    <div class="container-fluid d-flex text-center">
+      <a class="dropdown-item text-dark " href="#">
+        <img src={IG} height="25px" /> Instagram
+      </a>
+      <a class="dropdown-item text-dark " href="#">
+        <img src={FB} height="25px" /> Facebook
+      </a>
+      <a class="dropdown-item text-dark " href="#">
+        <img src={TW} height="25px" /> Twitter
+      </a>
+    </div>
+
+    <p class="text-dark mt-2">
+      Desarrollado por MyMusicShow Inc. 2022 - Todos los Derechos Reservados
+    </p>
+  </footer>
 );
--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,18 +2,65 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+  return (
+    <nav class="navbar navbar-expand-md bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand text-dark" href="#">
+          My music show
+        </a>
+        <button
+          class="navbar-toggler text-dark"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo02"
+          aria-controls="navbarTogglerDemo02"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle text-dark mx-5"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Proveedores
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item text-dark" href="#">
+                    Musica
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item text-dark" href="#">
+                    Fotografia y Video
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item text-dark" href="#">
+                    Locaciones
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item"></li>
+          </ul>
+          <form class="d-flex" role="search">
+            <a class="nav-link text-dark" href="#">
+              Registrate
+            </a>
+            <a class="nav-link text-dark" href="#">
+              Inicia sesion
+            </a>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 };
