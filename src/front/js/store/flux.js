d326b19941ff400ae1dbe273b649f6758214dac2
Jechf <jesuse.chf@gmail.com>
2022-08-24 14:29:35 +0000

    fix

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -74,7 +74,7 @@ const getState = ({ getStore, getActions, setStore }) => {
         try {
           // fetching data from the backend
           const resp = await fetch(
-            "https://3001-jechf-proyectofinal-kayqvlrtxml.ws-us62.gitpod.io/api/hello",
+            "https://3001-jechf-proyectofinal-kayqvlrtxml.ws-us63.gitpod.io/api/hello",
             opts
           );
           const data = await resp.json();
