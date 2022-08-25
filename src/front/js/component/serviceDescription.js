22560c1243c0e65007350ea35bb6674e13f7e017
Jechf <jesuse.chf@gmail.com>
2022-08-25 20:01:08 +0000

    presupuesto form

--- src/front/js/component/serviceDescription.js
+++ src/front/js/component/serviceDescription.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { useState } from "react";
-
+import "./services.css";
 export const ServiceDescription = () => {
   const [show, setShow] = useState(false);
   /*function handleAction (event) {
@@ -12,21 +12,15 @@ export const ServiceDescription = () => {
   }*/
   return (
     <>
-      <div className="container-fluid mt-3">
+      <div className="container-fluid mt-3 textfont">
         <h4>Descripcion del servicio</h4>
         <p className="text-wrap fs-6 text">
-          Situada a escasos minutos de Madrid, El Encanto D'Valeria os ofrece
-          todo lo que necesita vuestra boda para convertirse en el evento del
-          año. Con una trayectoria de más de tres décadas de experiencia en el
-          sector de la restauración, su equipo cuidará de hasta el más mínimo
-          detalle para que todo salga a pedir de boca. Espacios y capacidades La
-          finca dispone de unos espacios exteriores cargados de encanto en los
-          que podréis disfrutar de sus jardines, fuentes y, sobre todo, unas
-          espectaculares vistas, y en los que podréis llevar a cabo desde una
-          romántica ceremonia civil hasta un elegante cóctel de bienvenido o un
-          banquete en el que no falte absolutamente nada.
+          Oasis del descanso y la diversión Ubicado en la cima del Parque
+          Nacional Waraira Repano (El Ávila) se encuentra este hito de la
+          arquitectura moderna Venezolana, referencia del desarrollo urbano de
+          la ciudad de Caracas y del país.
         </p>
-        <button onClick={() => setShow(true)} className="btn btn-primary">
+        <button onClick={() => setShow(true)} className="btn boton btn-dark">
           Contratar servicio
         </button>
       </div>
---
+++ src/front/js/pages/presupuesto.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./providerProfile.css";
+
+export const Presupuesto = () => {
+  return (
+    <>
+      <div className="card p-4 justify-content-center textfont1">
+        <form className="mx-1 mx-md-4">
+          <h4>Solicitar Presupuesto</h4>
+          <input className="form-control m-2 btbr" placeholder="nombre" />
+          <input className="form-control m-2 btbr" placeholder="email" />
+          <input className="form-control m-2 btbr" placeholder="telefono" />
+          <input className="form-control m-2 btbr" type="date" />
+        </form>
+        <div className="d-flex justify-content-center mx-4 mb-2 mb-lg-4">
+          <button type="submit" className="mt-3 btn btn-dark w-50 btbr">
+            Enviar Solicitud
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
