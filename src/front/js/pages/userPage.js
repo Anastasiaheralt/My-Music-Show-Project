6f18262d7973e88123a293339c459e5e5b72688c
Anastasiaheralt <rosimarherrerao@gmail.com>
2022-08-26 22:25:33 +0000

    act img

--- src/front/js/pages/userPage.js
+++ src/front/js/pages/userPage.js
@@ -21,15 +21,17 @@ export const UserPage = () => {
           <div className="col-3">
             <div className="container">
               <div className="card" /*style="width: 18rem"*/>
-                <svg
+                <img
                   className="card-img-top"
                   src="https://us.123rf.com/450wm/thesomeday123/thesomeday1231712/thesomeday123171200009/91087331-icono-de-perfil-de-avatar-predeterminado-para-hombre-marcador-de-posici%C3%B3n-de-foto-gris-vector-de-ilu.jpg?ver=6"
                   alt="Card image cap"
                 >
                   {" "}
-                </svg>
+                </img>
                 <div className="card-body">
-                  <h5 className="card-title">{card.Title}</h5>
+                  <div className>
+                  <label htmlFor="disabledTextInput" className="form-label">
+                  <h5 className="card-title">{card.Title}</h5></label>
                   <p className="card-text">{card.Description}</p>
                 </div>
                 <div className="card-footer">
@@ -42,11 +44,6 @@ export const UserPage = () => {
           </div>
           <div className="col-9">
             <ProfileData />
-            <img
-              src="http://www.deepdiveintel.com/wp-content/uploads/2013/09/Owl-Eye-art-300x300.jpg"
-              className="img-thumbnail"
-              alt="..."
-            />
           </div>
         </div>
       </div>
