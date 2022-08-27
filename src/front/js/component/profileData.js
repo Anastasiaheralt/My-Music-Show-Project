a6419a57485359636b5c62c7674b722781cae5b1
Jechf <jesuse.chf@gmail.com>
2022-08-27 00:48:26 +0000

    prov

--- src/front/js/component/profileData.js
+++ src/front/js/component/profileData.js
@@ -1,57 +1,71 @@
 import React from "react";
+import "./register.css";
 
 export const ProfileData = () => {
   return (
-    <div>
-      <form>
-        <fieldset disabled>
-          <legend>Datos personales</legend>
-          <div className="mb-3">
-            <label htmlFor="disabledTextInput" className="form-label">
-              Nombres
-            </label>
-            <input
-              type="text"
-              id="disabledTextInput"
-              className="form-control"
-              placeholder="Disabled input"
-            />
-          </div>
-          <div class="mb-3">
-            <label htmlFor="disabledTextInput" className="form-label">
-              Apellidos
-            </label>
-            <input
-              type="text"
-              id="disabledTextInput"
-              className="form-control"
-              placeholder="Disabled input"
-            />
-          </div>
-          <div class="mb-3">
-            <label htmlFor="disabledTextInput" className="form-label">
-              Correo electronico
-            </label>
-            <input
-              type="text"
-              id="disabledTextInput"
-              className="form-control"
-              placeholder="Disabled input"
-            />
-          </div>
-          <div class="mb-3">
-            <label htmlFor="disabledTextInput" className="form-label">
-              Contrasena
-            </label>
-            <input
-              type="text"
-              id="disabledTextInput"
-              className="form-control"
-              placeholder="Disabled input"
-            />
-          </div>
-        </fieldset>
-      </form>
+    <div className="container d-flex fuente2">
+      <div className="col-6 ">
+        <form>
+          <fieldset disabled>
+            <legend>Datos personales</legend>
+            <div className="mb-3">
+              <label htmlFor="disabledTextInput" className="form-label">
+                Nombres
+              </label>
+              <input
+                type="text"
+                id="disabledTextInput"
+                className="form-control borde"
+                placeholder="Disabled input"
+              />
+            </div>
+            <div class="mb-3">
+              <label htmlFor="disabledTextInput" className="form-label">
+                Apellidos
+              </label>
+              <input
+                type="text"
+                id="disabledTextInput"
+                className="form-control borde"
+                placeholder="Disabled input"
+              />
+            </div>
+            <div class="mb-3">
+              <label htmlFor="disabledTextInput" className="form-label">
+                Correo electronico
+              </label>
+              <input
+                type="text"
+                id="disabledTextInput"
+                className="form-control borde"
+                placeholder="Disabled input"
+              />
+            </div>
+            <div class="mb-3">
+              <label htmlFor="disabledTextInput" className="form-label">
+                Contrasena
+              </label>
+              <input
+                type="text"
+                id="disabledTextInput"
+                className="form-control borde"
+                placeholder="Disabled input"
+              />
+            </div>
+          </fieldset>
+        </form>
+      </div>
+      <div className="col-6 m-3 p-3">
+        <div className="card">
+          <ul>
+            <h5>Eventos Contratados</h5>
+            <li>Eventos 1: Boda real lo que sea</li>
+            <li>Eventos 2: Boda real lo que sea</li>
+            <li>Eventos 3: Boda real lo que sea</li>
+            <li>Eventos 4: Boda real lo que sea</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
