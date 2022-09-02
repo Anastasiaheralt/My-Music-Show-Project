0ee9e8d008d7b568f5dbde8fea69bb0d83b17718
Jechf <jesuse.chf@gmail.com>
2022-09-02 21:29:34 +0000

    el ultimo commit

--- src/front/js/component/destacados.js
+++ src/front/js/component/destacados.js
@@ -33,9 +33,8 @@ export const Destacados = (props) => {
   const [modalData, setModalData] = useState([]);
 
   return (
-        <>
-
-<div
+    <>
+      <div
         className="modal fade"
         id="exampleModal3"
         tabIndex="-1"
@@ -47,7 +46,10 @@ export const Destacados = (props) => {
             <div className="modal-body px-5">
               <div className="text-center my-3">
                 <i className="fa-solid fa-gear fa-5x my-2"></i>
-                <h3> {`Usted a contratado a ${modalData.name} para el servicio de ${modalData.service}`} </h3>
+                <h3>
+                  {" "}
+                  {`Usted a contratado a ${modalData.name} para el servicio de ${modalData.service}`}{" "}
+                </h3>
               </div>
             </div>
             <div className="modal-footer">
@@ -64,50 +66,50 @@ export const Destacados = (props) => {
         </div>
       </div>
 
+      <div className="textfont">
+        <h1 className="text-white">Destacados</h1>
+        <div className="d-flex container-fluid ">
+          {store.providers.map((provider, index) => (
+            <div className="container-fluid pb-5">
+              <div className="row">
+                <div className="col">
+                  <div className="card" key={index}>
+                    <div className="card-header bg-white text-center">
+                      <img src={props.imagen + provider.id} />
+                    </div>
+                    <div className="card-body">
+                      <h5 className=" text-uppercase card-title">
+                        {provider.name}
+                      </h5>
+                      <h6 className=" fst-italic text-uppercase card-text">
+                        {provider.service}
+                      </h6>
 
+                      <div className="text-center">
+                        <button
+                          type="button"
+                          className="btn btn-dark boton mx-2"
+                          data-bs-toggle="modal"
+                          data-bs-target="#exampleModal3"
+                          onClick={() => {
+                            setModalData(provider),
+                              handleSubmitContrato({
+                                proveedor_id: provider.id,
+                                user_id: store.user.id,
+                              });
+                          }}
+                        >
+                          Contratar
+                        </button>
 
-
-
-    <div className="textfont">
-      <h1 className="text-white">Destacados</h1>
-      <div className="d-flex container-fluid ">
-        {store.providers.map((provider, index) => (
-          <div className="container-fluid pb-5">
-            <div className="row">
-              <div className="col">
-                <div className="card" key={index}>
-                  <div className="card-header bg-white text-center">
-                    <img src={props.imagen + provider.id} />
-                  </div>
-                  <div className="card-body">
-                    <h5 className=" text-uppercase card-title">
-                      {provider.name}
-                    </h5>
-                    <h6 className=" fst-italic text-uppercase card-text">
-                      {provider.service}
-                    </h6>
-                    
-                                <button
-                                  type="button"
-                                  className="btn btn-dark boton"
-                                  data-bs-toggle="modal"
-                                  data-bs-target="#exampleModal3"
-                                  onClick={() => { setModalData(provider), handleSubmitContrato({
-                                    proveedor_id: provider.id,
-                                    user_id: store.user.id,
-                                  }) }}
-                                >
-                                Contratar
-                                </button>
-                    
-                    <Link
-                      className="btn btn-dark boton"
-                      to={`/provider/${provider.id}`}
-                    >
-                      Detalles
-                    </Link>
-                    
-                    {/* <button
+                        <Link
+                          className="btn btn-dark boton"
+                          to={`/provider/${provider.id}`}
+                        >
+                          Detalles
+                        </Link>
+                      </div>
+                      {/* <button
                       className="btn status"
                       data-bs-toggle="modal"
                       data-bs-target="#exampleModal3"
@@ -119,7 +121,7 @@ export const Destacados = (props) => {
                       }
                     >Contratar</button> */}
 
-                    {/* <button
+                      {/* <button
                       className="btn status"
                       data-bs-toggle="modal"
                       data-bs-target="#exampleModal3"
@@ -127,7 +129,7 @@ export const Destacados = (props) => {
                         handleGetContratos()
                       }
                     >Ver Contratos Pendientes</button> */}
-                    {/* <button
+                      {/* <button
                       className="btn status"
                       data-bs-toggle="modal"
                       data-bs-target="#exampleModal3"
@@ -135,15 +137,14 @@ export const Destacados = (props) => {
                         handleGetPedidos()
                       }
                     >Ver Pedidos Pendientes</button> */}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
-</>  );
+    </>
+  );
 };
